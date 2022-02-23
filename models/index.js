351e8b680f40b1045c164268d19bb2b33d3ac750
Christina Bohn <christinajbohn@gmail.com>
2022-02-23 10:16:52 -0800

    Added custom helper to format date.

--- models/index.js
+++ models/index.js
@@ -1,37 +1,32 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-
-// User has many Posts
 User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
-// Post belongs to User (Post.user_id foreign key)
 Post.belongsTo(User, {
     foreignKey: 'user_id'
   });
 
-// User has many Comments
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
-// Comment belongs to User (Comment.user_id)
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
   });
 
-// Post has many Comments
 Post.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
   });
 
-// Comment belongs to Post (Comment.post_id)
 Comment.belongsTo(Post, {
     foreignKey: 'user_id'
   });
 
-module.exports = { User, Comment, Post };
+module.exports = { User, Post, Comment };
