55e74dcc1a9f674e6f2b0a366cc04ba19408530e
Christina Bohn <christinajbohn@gmail.com>
2022-03-03 23:28:48 -0800

    Added routes to get all/single posts to homeroutes.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,1 +1,10 @@
 // Add, edit, delete posts
+
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,8 +2,63 @@ const router = require("express").Router();
 const { User, Comment, Post } = require("../models");
 const withAuth = require("../utils/auth");
 
-//  homepage view - view all posts (not add/delete posts, comments)
+// HOMEPAGE VIEW - GET all posts
+router.get('/', async (req, res) => {
 
+    try {
+      const postData = await Post.findAll({
+        include: [ User ],
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('homepage', { 
+        posts,
+        logged_in: req.session.logged_in
+      });
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+  
+// GET a single post
+router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+  
+        include: [
+          User,
+          {
+            model: User,
+            attributes: ["username"],
+          },
+          {
+            model: Comment,
+            attributes: ["comment", "date_commented", "user_id"],
+            include: [
+              {
+                model: User,
+                attributes: ["username"],
+              },
+            ],
+          },
+        ],
+      });
+  
+      const post = postData.get({ plain: true });
+      const userPost = postData.user_id === req.session.user_id
+  
+      res.render('post', {
+        ...post,
+        userPost,
+        logged_in: req.session.logged_in
+      });
+      
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // LOGIN
 router.get("/login", (req, res) => {
@@ -14,29 +69,29 @@ router.get("/login", (req, res) => {
     }
   
     res.render("login");
-  });
+});
   
-  // LOGOUT
-  router.get("/logout", (req, res) => {
+// LOGOUT
+router.get("/logout", (req, res) => {
 
     if (!req.session.logged_in) {
-      res.redirect("/");
-      return;
+        res.redirect("/");
+        return;
     }
-  
+
     res.render("login");
-  });
-  
-  // SIGN UP
-  router.get("/signup", (req, res) => {
+});
+
+// SIGN UP
+router.get("/signup", (req, res) => {
 
     if (req.session.logged_in) {
-      res.redirect("/");
-      return;
+        res.redirect("/");
+        return;
     }
-  
+
     res.render("signup");
-  });
+});
 
 
 
