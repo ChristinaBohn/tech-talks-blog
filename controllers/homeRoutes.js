c29d5a69629455fe418310f7de722e01b4f16d53
Christina Bohn <christinajbohn@gmail.com>
2022-03-03 23:01:14 -0800

    Added routes for login, logout, and signup to home routes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,43 @@
-// Login, signup, homepage view - view all posts (not add/delete posts, comments)
+const router = require("express").Router();
+const { User, Comment, Post } = require("../models");
+const withAuth = require("../utils/auth");
+
+//  homepage view - view all posts (not add/delete posts, comments)
+
+
+// LOGIN
+router.get("/login", (req, res) => {
+
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  
+  // LOGOUT
+  router.get("/logout", (req, res) => {
+
+    if (!req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  
+  // SIGN UP
+  router.get("/signup", (req, res) => {
+
+    if (req.session.logged_in) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("signup");
+  });
+
+
+
+module.exports = router;
